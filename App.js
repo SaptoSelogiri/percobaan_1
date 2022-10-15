9b52eae7c97b9768ecfc7d5e6bfb3ddebe6f2289
SaptoSelogiri <yahones.tri@yahoo.com>
2022-10-15 18:10:15 +0700

    text input

--- App.js
+++ App.js
@@ -1,22 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert,Image,TextInput } from 'react-native';
 
-export default function App() {
-  return (
+
+
+
+export default function App(){
+
+  const [nama,setNama] =useState('sapto');
+  const [kodePos,setkodePos]= useState('57652');
+
+  return(
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! </Text>
-      <Text>dibuat oleh sapto</Text>
-       <Text>percobaan commit peertama</Text>
-      <StatusBar style="auto" />
+      <Text>tulis nama</Text>
+       <TextInput
+        style={styles.logo}
+        placeholder='e.g sapto'
+        onChangeText={(val)=>setNama(val)}/>
+        
+      <Text>tulis kode pos anda</Text>
+        <TextInput
+          style={styles.logo}
+          keyboardType= 'numeric'
+          placeholder='e.g.57652'
+          onChangeText={(val)=>setkodePos(val)}/>
+      <Button
+      title='INPUT'
+      color='green'
+      onPress={()=>Alert.alert('Data Input','Nama Anda '+nama+'\n Kode Pos yang Anda '+kodePos)}
+      />
+      
     </View>
-  );
-}
+   
+   
+    
+)}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+const styles =StyleSheet.create({
+  container :{
+    flex:1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems:'center',
+    justifyContent:'center'
   },
-});
+  logo :{
+    borderWidth:4,
+    borderColor:'blue',
+    pudding:8,
+    margin:10,
+    width:200
+  }
+})
